b7536329a2215d543a4cb0bafacaeb8713862060
daniel <13wrightd@gmail.com>
2016-02-08 23:13:40 -0500

    update

--- js/main.js
+++ js/main.js
@@ -5,8 +5,31 @@ $( document ).ready(function() {
 	});
 
 		
-	$(".button2").click(function() {
+	$(".button2").click(function() { 
 	  $(".left").css("background-color",$(".color-input").val());
+	  $(".color-input").val('');
+	});
+	$(".color-input").keyup(function (e) {
+    	if (e.keyCode == 13) {
+       		$(".left").css("background-color",$(".color-input").val());
+	  		$(".color-input").val('');
+   		}
+   	
+
+	});
+	$(".button3").click(function() { 
+	  $(".left").css("background-color",getRandomColor());
 	});
 
+
+
 });
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
